18ab166b8cd5a4f4bba6487d7c4757824a7522c0
limbail <limbail@hotmail.com>
2023-02-13 12:54:50 +0100

    gitignore change

--- ansible/runme.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-if __name__ == '__main__':
-    subprocess.call('cd /home/limbail/Projects/SapTool/ansible', shell=True)
-    subprocess.run(['pipenv', 'shell'])
-    subprocess.call('cd /home/limbail/Projects/SapTool/ansible', shell=True) 
-    subprocess.run(['ansible-playbook', '-i', 'my_inventory.ini', 'playbook.yml'])
-
-#dirty but work:P
-# with: pm2 start runme.py --name job_execute_playbook --interpreter python3 --restart-delay 60000
